c6ff2e61ae14e5df2e48a8f627e3f82286f50940
Fabián Novoa <fabiansteven90@gmail.com>
2017-05-08 14:33:39 -0500

    Renaming main.scala to calculator.scala

--- src/main/scala/calc/calculator.scala
+++ src/main/scala/calc/calculator.scala
@@ -5,5 +5,11 @@ object Calculator {
   def sum (a: Double , b : Double) : Double = a + b
   def subtract (a: Double, b:Double) : Double = a - b
   def mul  (a: Double, b:Double) : Double = a * b
-  def div  (a: Double, b:Double) : Double = a / b
+
+  def div(a: Double, b: Double): Double = {
+    if(b == 0) {
+      throw new RuntimeException("Cannot divide by zero")
+    }
+    return a./(b)
+  }
 }
